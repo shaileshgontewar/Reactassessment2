5b09e2ae51f0c82c83b89796a973133352735451
shailesh gontewar <shaileshsgontewar@gmail.com>
2022-05-18 17:55:08 +0530

    bg Change

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,30 +1,86 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+// import "./MyStyle.css";
 
-class form extends Component {
-  render() {
-    return (
+function Form() {
+  let [userName, setUserName] = useState("");
+  let [userRating, setUserRating] = useState("");
+  let [userDept, setUserDept] = useState("");
+  let [data, setData] = useState([]);
+
+  const submitHandler = (e) => {
+    console.log("submit");
+    e.preventDefault();
+    data.push({
+      name: userName,
+      dept: userDept,
+      rating: userRating,
+    });
+    setData(data);
+    setUserName("");
+    setUserDept("");
+    setUserRating("");
+    console.log(data);
+  };
+  return (
+    <>
       <div>
         <h1>Employee Feedback Form</h1>
-        <form>
+        <form onSubmit={submitHandler}>
           <div>
-            <label>Name :-</label>
-            <input type="text"></input>
+            <label htmlFor="name">Name :-</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              placeholder="Name"
+              autoComplete="off"
+            ></input>
           </div>
           <div>
-            <label>Department :-</label>
-            <input type="text"></input>
+            <label htmlFor="department">Department :-</label>
+            <input
+              type="text"
+              id="department"
+              name="department"
+              value={userDept}
+              onChange={(e) => setUserDept(e.target.value)}
+              placeholder="Department Name"
+              autoComplete="off"
+            ></input>
           </div>
           <div>
-            <label>Rating :-</label>
-            <input type="number"></input>
+            <label htmlFor="rating">Rating :-</label>
+            <input
+              type="number"
+              name="rating"
+              id="rating"
+              value={userRating}
+              onChange={(e) => setUserRating(e.target.value)}
+              placeholder="Rating"
+              autoComplete="off"
+            ></input>
           </div>
           <div className="submit">
-            {/* <input type="submit" /> */}
-            <button>Submit</button>
+            <button type="submit">Submit</button>
           </div>
         </form>
       </div>
-    );
-  }
+      <div className="datacontainer">
+        <div className="data">
+          {data.map((value, index) => {
+            return (
+              <div key={index} className="info">
+                Name :{value.name} | Department :{value.dept} | Rating :{" "}
+                {value.rating}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
-export default form;
+
+export default Form;
