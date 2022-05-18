d33921ce838e225e394d70cc5651a8cf3838f4d9
shailesh gontewar <shaileshsgontewar@gmail.com>
2022-05-18 09:54:25 +0530

    first commit

---
+++ src/Component/Form.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class form extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Employee Feedback Form</h1>
+        <form>
+          <div>
+            <label>Name :-</label>
+            <input type="text"></input>
+          </div>
+          <div>
+            <label>Department :-</label>
+            <input type="text"></input>
+          </div>
+          <div>
+            <label>Rating :-</label>
+            <input type="number"></input>
+          </div>
+          <div className="submit">
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default form;
